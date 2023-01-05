59c5381d633c822b6f82073b0ec508515c7ada33
Sai Suraj <ce20b031@iittp.ac.in>
2023-01-05 21:05:41 +0530

    Practiced more with itertools like group by function, count, cycle, and repeat (infinite type fns), and also wrote some comments to note some good points.

--- General/iter.py
+++ General/iter.py
@@ -35,3 +35,47 @@ import operator
 print(list(accumulate(a, func=operator.mul)))
 print(list(accumulate(a, func=max)))
 print(list(accumulate(a, func=min)))
+
+# New..🤔
+k = groupby(a, lambda x: x < 3)  # Grouping all elements with value <3
+
+for key, value in k:
+    print(key, ":", list(value))
+
+p = [{
+    "Age": 3,
+    "Name": "Suraj"
+}, {
+    "Age": 3,
+    "Name": "sfhdkas"
+}, {
+    "Age": 6,
+    "Name": "askjfdl"
+}]
+
+k1 = groupby(p, lambda x: x["Age"])
+
+for key, value in k1:
+    print(key, list(value))
+
+# count, cycle, repeat
+
+for i in count(
+        33
+):  # starts from a number and goes upto infinity if not stopper anywhere
+    if (i == 99):
+        break
+    print(i)
+
+# Iterates cyclically infinite times through the container until stopped
+cn = 0
+for i in cycle(a):
+    if (cn == 3):
+        break
+    print(i)
+    if (i == 5):
+        cn += 1
+
+for i in repeat(
+        a, 4):  # prints the container or data infinite times or unitl stopped
+    print(i)  # 2nd argument tells no.of times we need to repeat it.
